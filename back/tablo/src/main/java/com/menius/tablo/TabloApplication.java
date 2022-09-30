535641e77e6866f962fa33100a83a9d70255b79a
Andrei Sarateanu <sarateanuandrei28@gmail.com>
2022-09-30 15:41:29 +0300

    Back: Project Added

---
+++ back/tablo/src/main/java/com/menius/tablo/TabloApplication.java
@@ -0,0 +1,13 @@
+package com.menius.tablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabloApplication.class, args);
+    }
+
+}
