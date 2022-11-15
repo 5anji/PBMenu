cdad6764228261b8f91fbe9d04aca3dee43ee8eb
Andrei Sarateanu <sarateanuandrei28@gmail.com>
2022-11-15 14:22:18 +0200

    Response Status Changed

--- back/tablo/src/main/java/com/menius/tablo/controller/MenuHubController.java
+++ back/tablo/src/main/java/com/menius/tablo/controller/MenuHubController.java
@@ -13,17 +13,15 @@ import com.menius.tablo.service.FoodService;
 import com.menius.tablo.service.RestaurantService;
 import com.menius.tablo.service.SubsidiaryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-import static org.springframework.http.HttpStatus.ACCEPTED;
-import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
-@RequestMapping("api/menu")
+@RequestMapping("api")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class MenuHubController {
@@ -33,128 +31,128 @@ public class MenuHubController {
     private final DrinksService drinksService;
 
     // restaurant endpoints
-    @GetMapping("/restaurant/get-all/{pages}/{nrOfItems}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public List<RestaurantsGetResponseDto> getAllRestaurants(@PathVariable int pages, @PathVariable int nrOfItems) {
+    @GetMapping("/restaurant")
+    @ResponseStatus(OK)
+    public List<RestaurantsGetResponseDto> getAllRestaurants(@RequestParam int pages, @RequestParam int nrOfItems) {
         return restaurantService.getAllRestaurants(pages, nrOfItems);
     }
 
-    @PostMapping("/restaurant/save")
-    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/restaurant")
+    @ResponseStatus(OK)
     public void saveRestaurant(@RequestBody RestaurantsGetRequestDto restaurantsGetRequestDto) {
         restaurantService.saveRestaurant(restaurantsGetRequestDto);
     }
 
-    @DeleteMapping("/restaurant/delete/{restaurantId}")
-    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/restaurant/{restaurantId}")
+    @ResponseStatus(OK)
     public void deleteRestaurant(@PathVariable UUID restaurantId) {
         restaurantService.deleteRestaurantById(restaurantId);
     }
 
-    @GetMapping("/restaurant/get-by-id/{restaurantId}")
-    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping("/restaurant/{restaurantId}")
+    @ResponseStatus(OK)
     public RestaurantsGetResponseDto getRestaurant(@PathVariable UUID restaurantId) {
         return restaurantService.getRestaurantById(restaurantId);
     }
 
-    @PutMapping("/restaurant/detach/{restaurantId}")
-    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/restaurant/{restaurantId}")
+    @ResponseStatus(OK)
     public void detachRestaurant(@PathVariable UUID restaurantId) {
         restaurantService.detach(restaurantId);
     }
     // restaurant endpoints end
 
     //food endpoints
-    @GetMapping("/food/get-all/{pages}/{nrOfItems}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public List<FoodResponseDto> getAllFood(@PathVariable int pages, @PathVariable int nrOfItems) {
+    @GetMapping("/food")
+    @ResponseStatus(OK)
+    public List<FoodResponseDto> getAllFood(@RequestParam int pages, @RequestParam int nrOfItems) {
         return foodService.getAllFood(pages, nrOfItems);
     }
 
-    @PostMapping("/food/save")
-    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/food")
+    @ResponseStatus(OK)
     public void saveFood(@RequestBody FoodRequestDto foodRequestDto) {
         foodService.saveFood(foodRequestDto);
     }
 
-    @DeleteMapping("/food/delete/{foodId}")
-    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/food/{foodId}")
+    @ResponseStatus(OK)
     public void deleteFoodById(@PathVariable UUID foodId) {
         foodService.deleteFood(foodId);
     }
 
-    @GetMapping("/food/get-by-id/{foodId}")
-    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping("/food/{foodId}")
+    @ResponseStatus(OK)
     public FoodResponseDto getFoodById(@PathVariable UUID foodId) {
         return foodService.getFoodById(foodId);
     }
 
-    @GetMapping("/food/get-by-restaurantId/{restaurantId}/{pages}/{nrOfItems}")
-    @ResponseStatus(HttpStatus.FOUND)
-    public List<FoodResponseDto> getFoodByRestaurant(@PathVariable UUID restaurantId, @PathVariable int pages, @PathVariable int nrOfItems) {
+    @GetMapping("/restaurant/{restaurantId}/food")
+    @ResponseStatus(OK)
+    public List<FoodResponseDto> getFoodByRestaurant(@PathVariable UUID restaurantId, @RequestParam int pages, @RequestParam int nrOfItems) {
         return foodService.getFoodByRestaurant(restaurantId, pages, nrOfItems);
     }
 
-    @PutMapping("/food/detach/{foodId}")
-    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/food/{foodId}")
+    @ResponseStatus(OK)
     public void detachFood(@PathVariable UUID foodId) {
         foodService.detach(foodId);
     }
     //food endpoints end
 
     //subsidiary endpoints
-    @PostMapping("/subsidiary/save")
-    @ResponseStatus(CREATED)
+    @PostMapping("/subsidiary")
+    @ResponseStatus(OK)
     public void addSubsidiary(@RequestBody SubsidiaryAddRequestDto subsidiaryAddRequestDto) {
         subsidiaryService.addSubsidiary(subsidiaryAddRequestDto);
     }
 
-    @PutMapping("/subsidiary/detach/{subsidiaryId}")
-    @ResponseStatus(ACCEPTED)
+    @PutMapping("/subsidiary/{subsidiaryId}")
+    @ResponseStatus(OK)
     public void detachSubsidiary(@PathVariable UUID subsidiaryId) {
         subsidiaryService.detachSubsidiary(subsidiaryId);
     }
 
-    @GetMapping("/subsidiary/get-by-id/{subsidiaryId}")
-    @ResponseStatus(ACCEPTED)
+    @GetMapping("/subsidiary/{subsidiaryId}")
+    @ResponseStatus(OK)
     public SubsidiaryGetResponseDto getSubsidiaryById(@PathVariable UUID subsidiaryId) {
         return subsidiaryService.getSubsidiaryById(subsidiaryId);
     }
 
-    @GetMapping("/subsidiary/get-by-restaurnatID/{restaurantId}/{pages}/{nrOfItems}")
-    @ResponseStatus(ACCEPTED)
-    public List<SubsidiaryGetResponseDto> getSubsidiaryByRestaurantId(@PathVariable UUID restaurantId, @PathVariable int pages, @PathVariable int nrOfItems) {
+    @GetMapping("/restaurant/{restaurantId}/subsidiary")
+    @ResponseStatus(OK)
+    public List<SubsidiaryGetResponseDto> getSubsidiaryByRestaurantId(@PathVariable UUID restaurantId, @RequestParam int pages, @RequestParam int nrOfItems) {
         return subsidiaryService.getSubsidiaryByRestaurantId(restaurantId, pages, nrOfItems);
     }
     //subsidiary endpoints end
 
     //drink endpoints
-    @PostMapping("/drink/save")
-    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/drink")
+    @ResponseStatus(OK)
     public void saveDrink(@RequestBody DrinkRequestDto drinkRequestDto) {
         drinksService.saveDrink(drinkRequestDto);
     }
 
-    @DeleteMapping("/drink/delete/{drinkId}")
-    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/drink/{drinkId}")
+    @ResponseStatus(OK)
     public void deleteDrinkById(@PathVariable UUID drinkId) {
         drinksService.deleteDrink(drinkId);
     }
 
-    @GetMapping("/drink/get-by-id/{drinkId}")
-    @ResponseStatus(HttpStatus.FOUND)
+    @GetMapping("/drink/{drinkId}")
+    @ResponseStatus(OK)
     public DrinkGetResponseDto getDrinkById(@PathVariable UUID drinkId) {
         return drinksService.getDrinkById(drinkId);
     }
 
-    @GetMapping("/drink/get-by-restaurantId/{restaurantId}/{pages}/{nrOfItems}")
-    @ResponseStatus(HttpStatus.FOUND)
-    public List<DrinkGetResponseDto> getDrinkByRestaurant(@PathVariable UUID restaurantId, @PathVariable int pages, @PathVariable int nrOfItems) {
+    @GetMapping("/restaurant/{restaurantId}/drink")
+    @ResponseStatus(OK)
+    public List<DrinkGetResponseDto> getDrinkByRestaurant(@PathVariable UUID restaurantId, @RequestParam int pages, @RequestParam int nrOfItems) {
         return drinksService.getDrinkByRestaurant(restaurantId, pages, nrOfItems);
     }
 
-    @PutMapping("/drink/detach/{drinkId}")
-    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/drink/{drinkId}")
+    @ResponseStatus(OK)
     public void detachDrink(@PathVariable UUID drinkId) {
         drinksService.detachDrinkFromMenu(drinkId);
     }
